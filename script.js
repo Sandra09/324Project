6371c687a4c17890fff4079f3cf92a7f0b0752ba
Sandra Opakunle <sopakunle@luc.edu>
2023-04-24 03:29:52 -0500

    edits

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ dropdowns.forEach(dropdown => {
     const select = dropdown.querySelector('.select');
     const caret = dropdown.querySelector('.caret');
     const menu = dropdown.querySelector('.menu');
-    const options = dropdown.querySelector('.menu li');
+    const options = dropdown.querySelectorAll('.menu li');
     const selected = dropdown.querySelector('.selected');
 
     select.addEventListener('click', () => {
@@ -31,13 +31,90 @@ dropdowns.forEach(dropdown => {
     });
 });
 
+//image gallery
 function myFunction(smallImg){
     var fullImg = document.getElementById("imageBox1");
     fullImg.src = smallImg.src;
     fullImg.title = smallImg.title;
-}
+};
 function myFunction1(smallImg){
     var fullImg = document.getElementById("imageBox2");
     fullImg.src = smallImg.src;
-    fullImg.title = smallImg.title;
+};
+
+//misc slider
+//Get elements from the DOM
+const slider = document.querySelector('.image-slider');
+const arrLeft = document.querySelector('.arr-left');
+const arrRight = document.querySelector('.arr-right');
+const heading = document.querySelector('.caption h1');
+const description = document.querySelector('.caption p');
+
+const images = [
+    "Artinstituteofchicago.jpg",
+    "CentennialWheelchicago.webp",
+    "Chicagomuseumofice-cream.jpg",
+    "Dusablemuseum.webp",
+    "LoyolaUniversityMuseumofArt.jpg",
+    "MuseumofContemporaryChicago.jpg",
+    "Museumofcontemporaryphotography.jpg",
+    "MuseumofMexicanart.webp",
+    "Nationalveteransartmuseum.jpg",
+    "Thefieldmuseum.jpg"
+];
+
+const headings = [
+    "Art Institute of Chicago",
+    "Centennial Wheel chicago",
+    "Chicago Museum of Ice Cream",
+    "Dusable Museum",
+    "Loyola University Museum of Art",
+    "Museum of Contemporary Arts Chicago",
+    "Museum of Contemporary Photography",
+    "Museum of Mexican Art",
+    "National Veterans Art Museum",
+    "The field Museum"
+];
+
+const descriptions = [
+    "Discover paintings, drawings, sculpture, and design spanning the ancient to the contemporary world",
+    "Soar to heights of nearly 200 feet on the iconic Centennial Wheel",
+    "An ice cream fantasy of endless treats and playtime for all ages",
+    "The Nation's Oldest Independent African American Museum",
+    "Your Loyola Experience starts here",
+    "Where new ideas are shown and tested",
+    "Boldly presents thought-provoking photography through groundbreaking exhibitions and programming",
+    "Recreation of the legacy left behind by Native cultures before Spanish colonizers arrived to present-day Mexico",
+    "The impact of war through the collection, preservation and exhibition of art by military veterans",
+    "Connects all of us to the natural world and the human story"
+];
+
+let id = 0;
+
+//The slider function
+function slide(id){
+    //set background image
+    slider.style.backgroundImage = "url(Misc/${images[id]})";
+    //change heading
+    heading.innerText = headings[id];
+    //change description
+    description.innerText = descriptions[id];
+
+    //click arrows
+    arrLeft.addEventListener('click', () => {
+        //decrement image id
+        id--;
+        if(id < 0){
+            id = images.length - 1;
+        }
+        slide(id);
+    });
+    arrRight.addEventListener('click', () => {
+        //increment image id
+        id++;
+        if(id > images.length - 1){
+            id = 0;
+        }
+        slide(id);
+    });
 }
